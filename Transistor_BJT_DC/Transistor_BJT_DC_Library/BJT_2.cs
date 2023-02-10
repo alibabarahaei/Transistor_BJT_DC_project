54bced1ae1224b96b6a26f05aef6c7c8f5ab6a2f
MHB80 <80700259+MHB80@users.noreply.github.com>
2023-02-10 23:37:30 +0330

    Update BJT_2.cs
    ch1

--- Transistor_BJT_DC/Transistor_BJT_DC_Library/BJT_2.cs
+++ Transistor_BJT_DC/Transistor_BJT_DC_Library/BJT_2.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +9,8 @@ namespace Transistor_BJT_DC.Transistor_BJT_DC_Library
 
     public class BJT_2: BJT
     {
+        public bool istransistor_PNP = false;      //false  --->  PNP       true  --->  NPN
+
         public double beta;
         public double hfe;
         //resistance
@@ -21,6 +23,9 @@ namespace Transistor_BJT_DC.Transistor_BJT_DC_Library
         //resistance
         //voltage
         public double VBE;
+
+        public double VB;
+
         public double VBC;
         public double VCE;
         public double VCC;
@@ -37,7 +42,10 @@ namespace Transistor_BJT_DC.Transistor_BJT_DC_Library
         public bool Check_Active_Mode()
         {
             //default in active mode
-            VBE = 0.7;
+            
+            
+            if(istransistor_PNP==false){VBE = 0.7;}
+            else{VBE = -0.7;}
             //default in active mode
 
             IB = (VCC - VBE) / (RB + ((beta + 1) * RC)); // With considering IB . 
@@ -46,22 +54,44 @@ namespace Transistor_BJT_DC.Transistor_BJT_DC_Library
             VCE = (VCC) - (((beta + 1) / beta) * (IC * RC)) - (IE * RE);
             VC = (VCC) - (((beta + 1) / beta) * (IC * RC));
             VE = IE * RE;
-
-            if (VCE >= 0.2 && IB > 0 && IC > 0 && IE >= 0)
+            if (istransistor_PNP==false)
             {
+                if (VCE >= 0.2 && IB > 0 && IC > 0 && IE >= 0)
+                    {
                 return true;
+                    }
+                else
+                    {
+                return false;
+                    }
             }
             else
             {
-                return false;
+                if (VCE <= -0.2 && IB < 0 && IC < 0 && IE < 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
             }
+
 
         }
         public bool Check_Saturation_Mode()
         {
             //default in saturation mode
+            if(istransistor_PNP==false){      
             VBE = 0.8;
             VCE = 0.2;
+            }
+            else
+            {
+                VBE = -0.8;
+                VCE = -0.2;
+            }
+
             //default in saturation mode
 
             IE = 0.0;
@@ -69,7 +99,7 @@ namespace Transistor_BJT_DC.Transistor_BJT_DC_Library
             IC = (VCC - VCE - RE * IE) / (((beta + 1) / beta) * RC);
             IBmin = IC / hfe;
             VBC = VBE - VCE;
-
+            if(istransistor_PNP==false){     
             if (IB > IBmin && IB > 0 && IC > 0 && IE >= 0)
             {
                 return true;
@@ -78,19 +108,59 @@ namespace Transistor_BJT_DC.Transistor_BJT_DC_Library
             {
                 return false;
             }
+            else
+            {
+                if (IB < IBmin && IB < 0 && IC < 0 && IE < 0)
+                    {
+                    return true;
+                    }
+                else
+                    {
+                    return false;
+                    }
+            }
+            }
+
 
         }
         public bool Check_CutOff_Mode()
-        {
-            VBE = 0.7;
-            if (VBE < 0.7) //Not Sure!
+        {   
+            //assume that we are in the active mode
+            if (istransistor_PNP==false)
             {
-                return true;
+                VBE = -0.7;
             }
             else
             {
+                VBE = 0.7; 
+            }
+
+            IB = (VCC - VBE) / (RB + ((beta + 1) * RC)); // With considering IB . 
+
+            VBE = VCC - IB*(RB + (beta + 1) * RC) ;
+            if (istransistor_PNP==false)
+            {
+                if (VBE < 0.7 || IB == 0) 
+            {
+                return true;
+            }
+                else
+            {
                 return false;
             }
+            }
+            else
+            {
+                if (VBE >= 0.7 || IB == 0) 
+            {
+                return true;
+            }
+                else
+            {
+                return false;
+            }
+            }
+
 
         }
 
