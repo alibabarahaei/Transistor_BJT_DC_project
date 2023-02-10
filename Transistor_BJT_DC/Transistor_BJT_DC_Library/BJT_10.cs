ac0267bd6cd4c3a1bb6e4d71b5fee4a8beede9dc
MHB80 <80700259+MHB80@users.noreply.github.com>
2023-02-10 23:35:30 +0330

    Update BJT_10.cs
    ch1

--- Transistor_BJT_DC/Transistor_BJT_DC_Library/BJT_10.cs
+++ Transistor_BJT_DC/Transistor_BJT_DC_Library/BJT_10.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +9,8 @@ namespace Transistor_BJT_DC.Transistor_BJT_DC_Library
 
     public class BJT_10: BJT
     {
+        public bool istransistor_PNP==false;
+
         public double beta;
         public double hfe;
         //resistance
@@ -27,6 +29,7 @@ namespace Transistor_BJT_DC.Transistor_BJT_DC_Library
         public double VCC;
         public double VC;
         public double VE;
+        public double VE;
         public double VTH;
         //voltage
         //current
@@ -38,7 +41,9 @@ namespace Transistor_BJT_DC.Transistor_BJT_DC_Library
         public bool Check_Active_Mode()
         {
             //default in active mode
-            VBE = 0.7;
+            if(istransistor_PNP==false){VBE = 0.7;}
+            else{VBE = -0.7;}
+
             //default in active mode
 
             IC = (beta / (beta + 1)) * ((VEE - VBE) / (RE + (RB / (beta + 1))));
@@ -47,7 +52,7 @@ namespace Transistor_BJT_DC.Transistor_BJT_DC_Library
             VC = (VCC) - (IC * RC);
             VE = -VBE - (IB * RB);
             VCE = VC - VE;
-
+            if(istransistor_PNP==false){
             if (VCE >= 0.2 && IB > 0 && IC > 0 && IE > 0)
             {
                 return true;
@@ -56,13 +61,31 @@ namespace Transistor_BJT_DC.Transistor_BJT_DC_Library
             {
                 return false;
             }
+            }
+            else{
+                if (VCE <= -0.2 && IB < 0 && IC < 0 && IE < 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            
+
 
         }
         public bool Check_Saturation_Mode()
         {
             //default in saturation mode
+            if(istransistor_PNP==false){           
             VBE = 0.8;
-            VCE = 0.2;
+            VCE = 0.2;}
+            else{
+            VBE =-0.8;
+            VCE = -0.2;
+            }
             //default in saturation mode
 
             IB = (VEE - VBE - (IE * RE)) / (RB);
@@ -70,7 +93,7 @@ namespace Transistor_BJT_DC.Transistor_BJT_DC_Library
             IE = (VEE - VBE - (IB * RB)) / (RE);
             IBmin = IC / hfe;
             VBC = VBE - VCE;
-
+            if(istransistor_PNP==false){
             if (IB > IBmin && IB > 0 && IC > 0 && IE > 0)
             {
                 return true;
@@ -79,12 +102,35 @@ namespace Transistor_BJT_DC.Transistor_BJT_DC_Library
             {
                 return false;
             }
+            }
+            else{
+                if (IB < IBmin && IB < 0 && IC < 0 && IE < 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
 
         }
         public bool Check_CutOff_Mode()
         {
-            VBE = 0.7;
-            if (VBE < 0.7) //Not Sure!
+            if(istransistor_PNP==false){
+                VBE=0.7;
+            }
+            else
+            {
+                VBE = -0.7;
+            }
+            IC = (beta / (beta + 1)) * ((VEE - VBE) / (RE + (RB / (beta + 1))));
+            IB = IC / beta;
+            VBE = (IB * RB) - (IE * RE) - VEE
+            if (istransistor_PNP==false)
+            {
+            if (VBE < 0.7 || IC == 0) //Not Sure!
             {
                 return true;
             }
@@ -92,6 +138,19 @@ namespace Transistor_BJT_DC.Transistor_BJT_DC_Library
             {
                 return false;
             }
+            }
+            else
+            {
+            if (VBE > -0.7 || IC == 0) //Not Sure!
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            }
+
 
         }
 
