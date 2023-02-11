8f2902ad53e4d5b1cc29007ae83dd40ea061b847
MHB80 <80700259+MHB80@users.noreply.github.com>
2023-02-12 00:16:18 +0330

    Add files via upload

--- Transistor_BJT_DC/Transistor_BJT_DC_Library/BJT_10.cs
+++ Transistor_BJT_DC/Transistor_BJT_DC_Library/BJT_10.cs
@@ -1,9 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace Transistor_BJT_DC.Transistor_BJT_DC_Library
 {
 
-    public class BJT_10 : BJT
+    public class BJT_10: BJT
     {
-        public bool istransistor_PNP = false;
+        public bool istransistor_PNP==false;
 
         public double beta;
         public double hfe;
@@ -23,6 +29,7 @@ namespace Transistor_BJT_DC.Transistor_BJT_DC_Library
         public double VCC;
         public double VC;
         public double VE;
+
         public double VTH;
         //voltage
         //current
@@ -34,8 +41,8 @@ namespace Transistor_BJT_DC.Transistor_BJT_DC_Library
         public bool Check_Active_Mode()
         {
             //default in active mode
-            if (istransistor_PNP == false) { VBE = 0.7; }
-            else { VBE = -0.7; }
+            if(istransistor_PNP==false){VBE = 0.7;}
+            else{VBE = -0.7;}
 
             //default in active mode
 
@@ -45,19 +52,17 @@ namespace Transistor_BJT_DC.Transistor_BJT_DC_Library
             VC = (VCC) - (IC * RC);
             VE = -VBE - (IB * RB);
             VCE = VC - VE;
-            if (istransistor_PNP == false)
+            if(istransistor_PNP==false){
+            if (VCE >= 0.2 && IB > 0 && IC > 0 && IE > 0)
             {
-                if (VCE >= 0.2 && IB > 0 && IC > 0 && IE > 0)
-                {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
+                return true;
             }
             else
             {
+                return false;
+            }
+            }
+            else{
                 if (VCE <= -0.2 && IB < 0 && IC < 0 && IE < 0)
                 {
                     return true;
@@ -67,22 +72,19 @@ namespace Transistor_BJT_DC.Transistor_BJT_DC_Library
                     return false;
                 }
             }
-
+            
 
 
         }
         public bool Check_Saturation_Mode()
         {
             //default in saturation mode
-            if (istransistor_PNP == false)
-            {
-                VBE = 0.8;
-                VCE = 0.2;
-            }
-            else
-            {
-                VBE = -0.8;
-                VCE = -0.2;
+            if(istransistor_PNP==false){           
+            VBE = 0.8;
+            VCE = 0.2;}
+            else{
+            VBE =-0.8;
+            VCE = -0.2;
             }
             //default in saturation mode
 
@@ -91,19 +93,17 @@ namespace Transistor_BJT_DC.Transistor_BJT_DC_Library
             IE = (VEE - VBE - (IB * RB)) / (RE);
             IBmin = IC / hfe;
             VBC = VBE - VCE;
-            if (istransistor_PNP == false)
+            if(istransistor_PNP==false){
+            if (IB > IBmin && IB > 0 && IC > 0 && IE > 0)
             {
-                if (IB > IBmin && IB > 0 && IC > 0 && IE > 0)
-                {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
+                return true;
             }
             else
             {
+                return false;
+            }
+            }
+            else{
                 if (IB < IBmin && IB < 0 && IC < 0 && IE < 0)
                 {
                     return true;
@@ -118,9 +118,8 @@ namespace Transistor_BJT_DC.Transistor_BJT_DC_Library
         }
         public bool Check_CutOff_Mode()
         {
-            if (istransistor_PNP == false)
-            {
-                VBE = 0.7;
+            if(istransistor_PNP==false){
+                VBE=0.7;
             }
             else
             {
@@ -128,28 +127,28 @@ namespace Transistor_BJT_DC.Transistor_BJT_DC_Library
             }
             IC = (beta / (beta + 1)) * ((VEE - VBE) / (RE + (RB / (beta + 1))));
             IB = IC / beta;
-            VBE = (IB * RB) - (IE * RE) - VEE;
-            if (istransistor_PNP == false)
+            IE = IB + IC;
+            if (istransistor_PNP==false)
             {
-                if (VBE < 0.7 || IC == 0) //Not Sure!
-                {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
+            if ( IE == 0 || VEE < 0.7) //Not Sure!
+            {
+                return true;
             }
             else
             {
-                if (VBE > -0.7 || IC == 0) //Not Sure!
-                {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
+                return false;
+            }
+            }
+            else
+            {
+            if (IE == 0) //Not Sure!
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
             }
 
 
